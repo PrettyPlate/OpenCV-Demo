9b9cb3bd25ed2b125b38f693767f450b5639ce46
PrettyPlate <843434046@qq.com>
2022-03-15 16:21:03 +0800

    commit

---
+++ build/src/CMakeFiles/OPENCVDEMO.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OPENCVDEMO.dir/main.cpp.o"
+  "../../bin/OPENCVDEMO.pdb"
+  "../../bin/OPENCVDEMO"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/OPENCVDEMO.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
